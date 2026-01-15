2c82ae715d5df5999561111ca968e10a46753ec0
t5ugu <58124892+t5ugu@users.noreply.github.com>
2026-01-15 06:34:45 +0000

    コメント追加と、より扱いやすく。

--- animate.h
+++ animate.h
@@ -1,9 +1,12 @@
 #include "shape.h"
+// 内部では、秒は用いず、フレーム単位で扱うこと
 #define FPS 20
+// 何フレームか
 #define FRAME_t int
+// drawCircle には、このまま入れてよい
 #define BALL_RAD 10 //px
 
 // 0  <= t <= crashTime: つぶれている; 中心は (a.x, a.y+a.h)
 // cT <= t <= bounceTime: 上に凸の山を描いている; 最高点 (a.x + a.w/2, a.y)
 // bT <= t <= bT+cT: つぶれている; 中心は (a.x+a.w, a.y+a.h)
-void drawBall(CANVAS c, FRAME_t crashTime, FRAME_t bounceTime, FRAME_t t, struct atlas a);
+void animateBall(CANVAS c, FRAME_t crashTime, FRAME_t bounceTime, FRAME_t t, struct atlas a);
--- shape.h
+++ shape.h
@@ -1,4 +1,4 @@
-struct color { char red, green, blue; };
+struct color { unsigned char r, g, b; };
 
 #define CANVAS_WIDTH 400
 #define CANVAS_HEIGHT 300
@@ -8,15 +8,22 @@ struct color { char red, green, blue; };
 // 左から x px, 上から y px, 右向きに h px, 下向きに w px
 struct atlas { int x, y, h, w; };
 
-// 中心点(x, y)、半径 radius の円
-void drawCircle(CANVAS c, int x, int y, int radius, struct color clr) {
-    drawEllipse(c, {x - radius, y - radius, 2 * radius, 2 * radius}, clr);
+void drawPixel(CANVAS c, int x, int y, struct color clr) {
+    c[x][y] = clr;
 }
 
+// 中心点(x, y)、半径 radius の円
+void drawCircle(CANVAS c, int x, int y, int radius, struct color clr) {
+    drawEllipse(c, (struct atlas){x - radius, y - radius, 2 * radius, 2 * radius}, clr);
+}
+
+// 長方形
 void drawRectangle(CANVAS c, struct atlas a, struct color clr);
 
+// 傾きのない楕円
 void drawEllipse(CANVAS c, struct atlas a, struct color clr);
 
+// 二等辺三角形
 void drawIsocelesTriangle(CANVAS c, struct atlas a, struct color clr);
 
 // 点(x, y) を原点とする波。
